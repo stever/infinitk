046048a8b44467c231c1dfecc3a39f899c7deab3
Steven Robertson <steverobertson@gmail.com>
2018-09-03 19:06:28 +0100

    Customising font on xterm.js example

--- node/moonpad/src/js/index.js
+++ node/moonpad/src/js/index.js
@@ -1,5 +1,17 @@
 import { Terminal } from 'xterm';
+import * as fit from 'xterm/lib/addons/fit/fit';
 
-const term = new Terminal();
-term.open(document.getElementById('terminal'));
-term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+Terminal.applyAddon(fit);
+
+const xterm = new Terminal({
+    fontFamily: 'PragmataPro Mono',
+    fontSize: 16,
+    cursorBlink: true
+});
+
+xterm.open(document.getElementById('terminal'));
+xterm.fit();
+
+// TODO: Re-fit on resize.
+
+xterm.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
